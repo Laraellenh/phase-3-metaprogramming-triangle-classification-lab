a27a12eefe42c85622a3e8978923efbc5e562ef0
Lara <laraellenh@gmail.com>
2022-03-06 15:54:19 -0500

    commit tree 573070c5b8f191e1dfd0d1201a9337d830743c43

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
+require "pry"
+
 class Triangle
+
+
+  attr_accessor :a, :b, :c, :triangle
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @triangle = [a, b, c, ]
+  end
   # write code here
+  def kind 
+    if self.invalid?
+      raise TriangleError
+    elsif @a ==@b && @b == @c
+      :equilateral
+    elsif @a==@b||@b==@c||@c==@a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def invalid?
+    if self.triangle.min <= 0 || @a + @b<=@c || @b+@c<=@a || @c+@a<=@b
+      true
+    end
+  end
+    
+  class TriangleError < StandardError
+    def message
+      "you died"
+    end
+  end
+
+
+
+
 end
